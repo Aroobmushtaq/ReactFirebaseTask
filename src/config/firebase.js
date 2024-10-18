7cead3832b7a5f569338696c1efd20b643c0bee8
AroobMushtaq <aroobmushtaq786@gmail.com>
2024-10-18 18:46:20 +0500

    Updated Code

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,7 @@ import { getAuth,GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-  apiKey: "AIzaSyDGsFjH93Z525eIhnkpbwzVCMPH7Jdp1gs",
-  authDomain: "new-project-88dbe.firebaseapp.com",
-  projectId: "new-project-88dbe",
-  storageBucket: "new-project-88dbe.appspot.com",
-  messagingSenderId: "651262056182",
-  appId: "1:651262056182:web:21a4055fd5fa892058fed7",
-  measurementId: "G-13Y95PX34T", 
-  databaseURL: "https://new-project-88dbe-default-rtdb.firebaseio.com",
+  
 };
 
 const app = initializeApp(firebaseConfig);
